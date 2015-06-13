d2603d88f43559e6252966a85c43212672bb622b
佐藤一輝 <i.tennis0210@gmail.com>
2015-06-13 14:20:15 +0900

    ログイン画面作成

---
+++ CoRee/RecommendViewController.swift
@@ -0,0 +1,25 @@
+//
+//  RecommendViewController.swift
+//  CoRee
+//
+//  Created by 佐藤一輝 on 2015/06/13.
+//  Copyright (c) 2015年 validtationError. All rights reserved.
+//
+
+import UIKit
+
+class RecommendViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
+
